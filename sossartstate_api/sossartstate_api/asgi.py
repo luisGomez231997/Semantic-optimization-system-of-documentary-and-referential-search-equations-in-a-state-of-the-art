49aec90065d591503925d5052d407e64d5271b02
luisGomez231997 <luis.alberto.gomez@correounivalle.edu.co>
2021-01-11 23:35:53 -0500

    base api project and package to make PLN and manage bibtext files

--- sossartstate_api/sossartstate_api/asgi.py
+++ sossartstate_api/sossartstate_api/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for sossartstate project.
+ASGI config for sossartstate_api project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sossartstate.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sossartstate_api.settings')
 
 application = get_asgi_application()
